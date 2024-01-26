5a4ff18e4e6f3c0698bfb426090801c55bbb350e
Igorth <igordiasth@gmail.com>
2024-01-26 13:11:51 -0500

    Added small Modal project

--- js-before-react/main.js
+++ js-before-react/main.js
@@ -1,2 +1,15 @@
-const age = null;
-document.body.innerText = `Your age is ${age ?? 'Não Informado'}`;
+// Nullish Coalescing Operator
+// const age = null;
+// document.body.innerText = `Your age is ${age ?? 'Não Informado'}`;
+
+// Objects
+const user = {
+  name: 'John Doe',
+  age: 23,
+  address: {
+    city: 'New York',
+    state: 'NY',
+  },
+};
+
+document.body.innerText = JSON.stringify(user);

efb4f8ee8bf9b9da9b30009b7600956915cbc04e
Igorth <igordiasth@gmail.com>
2024-01-27 22:11:49 -0500

    Added types of functions

---
+++ fundamentals-part2/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function logger() {
+  console.log('H');
+}
+// calling / running / invoking the function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// Function declaration
+function calcAge1(birthYear) {
+  return 2034 - birthYear;
+}
+const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+  return 2034 - birthYear;
+};
+const age2 = calcAge2(1991);
+console.log(age2);
+
+// Arrow function
+const calcAge3 = birthYear => 2034 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2024 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years.`;
+};
+console.log(yearsUntilRetirement(1991, 'John'));

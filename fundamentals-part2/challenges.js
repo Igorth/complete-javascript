17eae137a4fc7c350e06d6c614f3be173c001f24
Igorth <igordiasth@gmail.com>
2024-01-30 11:43:54 -0500

    Added challenged 4 arrays

--- fundamentals-part2/challenges.js
+++ fundamentals-part2/challenges.js
@@ -109,36 +109,85 @@ the corresponding tip, calculated based on the rules above
  * Mark Miller's (23.9)!".
  */
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
 
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
+
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+// mark.calcBMI();
+// john.calcBMI();
+
+// if (mark.bmi > john.bmi) {
+//   console.log(
+//     `${mark.fullName} is a BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})`
+//   );
+// } else {
+//   console.log(
+//     `${john.fullName} is a BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})`
+//   );
+// }
+
+////////////////////////////////////////////////////////////////////
+
+// Challenge 4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
+/**
+ * Create an array called bills containing all 10 test bill values.
+ */
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
-mark.calcBMI();
-john.calcBMI();
+/**
+ * Create empty arrays for the tips and the totals (tips and totals)
+ */
+const tips = [];
+const totals = [];
 
-if (mark.bmi > john.bmi) {
-  console.log(
-    `${mark.fullName} is a BMI (${mark.bmi}) is higher than ${john.fullName} (${john.bmi})`
-  );
-} else {
-  console.log(
-    `${john.fullName} is a BMI (${john.bmi}) is higher than ${mark.fullName} (${mark.bmi})`
-  );
+/**
+ * Use the calcTip function we wrote before (included in the
+ * starter code) to calculate tips and total values (bill + tip)
+ * for every bill value in the bills array. Use a for loop to
+ * perform the 10 calculations!
+ */
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  const total = bills[i] + tip;
+  tips.push(tip);
+  totals.push(total);
 }
+console.log(tips);
+console.log(totals);
+
+/**
+ * Write a function calcAverage which takes an array called arr
+ * as an argument. This function calculates the average of
+ * all numbers in the given array.
+ */
+
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calcAverage(totals));
--- fundamentals-part2/script.js
+++ fundamentals-part2/script.js
@@ -297,19 +297,19 @@
 ////////////////////////////////////////////////////////////////////
 
 // WHILE LOOPS
-let rep = 1;
-while (rep <= 5) {
-  console.log(`Repetition ${rep}`);
-  rep++;
-}
+// let rep = 1;
+// while (rep <= 5) {
+//   console.log(`Repetition ${rep}`);
+//   rep++;
+// }
 
-let dice = Math.trunc(Math.random() * 6) + 1;
+// let dice = Math.trunc(Math.random() * 6) + 1;
 
-while (dice !== 6) {
-  console.log(`Dice ${dice}`);
-  dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//   console.log(`Dice ${dice}`);
+//   dice = Math.trunc(Math.random() * 6) + 1;
 
-  if (dice === 6) {
-    console.log('You rolled a 6!');
-  }
-}
+//   if (dice === 6) {
+//     console.log('You rolled a 6!');
+//   }
+// }

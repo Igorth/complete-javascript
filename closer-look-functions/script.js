b6d38d4fc4dc5df24d24191d898d49c2afe454ba
Igorth <igordiasth@gmail.com>
2024-02-01 05:43:57 -0500

    Initial commit more functions

--- closer-look-functions/script.js
+++ closer-look-functions/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 55 * numPassengers
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('1234');
+createBooking('5678', 2, 100);
+createBooking('1234', undefined, 100);
+createBooking('1234', 8);

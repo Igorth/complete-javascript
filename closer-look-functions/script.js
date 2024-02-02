f845118fee6f835c2a5ff919e16404142635d9a3
Igorth <igordiasth@gmail.com>
2024-02-02 15:45:13 -0500

    feat: add a AJAX call to a coutries API

--- closer-look-functions/script.js
+++ closer-look-functions/script.js
@@ -60,32 +60,31 @@
 
 /////////////////////////////////////////////////////////////////////////////
 
-const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
-};
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const upperFirstWord = function (str) {
-  const [first, ...rest] = str.split(' ');
-  return [first.toUpperCase(), ...rest].join(' ');
-};
+// const upperFirstWord = function (str) {
+//   const [first, ...rest] = str.split(' ');
+//   return [first.toUpperCase(), ...rest].join(' ');
+// };
 
-const transformer = function (str, fn) {
-  console.log(`Original string: ${str}`);
-  console.log(`Transformed string: ${fn(str)}`);
-  console.log(`Transformed by: ${fn.name}`);
-};
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+//   console.log(`Transformed by: ${fn.name}`);
+// };
 
-// Callback function, we tell javascript to call them later
-transformer('hello world', upperFirstWord);
-console.log('------------------------------------------');
-transformer('hello world', oneWord);
+// // Callback function, we tell javascript to call them later
+// transformer('hello world', upperFirstWord);
+// console.log('------------------------------------------');
+// transformer('hello world', oneWord);
 
-const high5 = function () {
-  console.log('Hight 5');
-};
-// Calback function
-document.body.addEventListener('click', high5);
+// const high5 = function () {
+//   console.log('Hight 5');
+// };
+// // Calback function
+// document.body.addEventListener('click', high5);
 
-const array = ['Jonas', 'Maria', 'J'];
-
-array.forEach(high5);
+// const array = ['Jonas', 'Maria', 'J'];
+// array.forEach(high5);

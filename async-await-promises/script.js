5552a322998bd546e05c0a208f82298b5911d160
Igorth <igordiasth@gmail.com>
2024-02-03 14:45:13 -0500

    feat: consuming promises

--- async-await-promises/script.js
+++ async-await-promises/script.js
@@ -52,36 +52,48 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const getCountryAndNeighbour = function (country) {
-  // AJAX call country 1
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   // AJAX call country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', () => {
-    const [data] = JSON.parse(request.responseText);
-    console.log(data);
+//   request.addEventListener('load', () => {
+//     const [data] = JSON.parse(request.responseText);
+//     console.log(data);
 
-    // Render country
-    renderCountry(data);
+//     // Render country
+//     renderCountry(data);
 
-    // Get neighbour country
-    const neighbour = data.borders?.[0];
+//     // Get neighbour country
+//     const neighbour = data.borders?.[0];
 
-    // AJAX call country 2
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-    request2.send();
+//     // AJAX call country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
 
-    request2.addEventListener('load', () => {
-      const [data2] = JSON.parse(request2.responseText);
-      console.log(data2);
+//     request2.addEventListener('load', () => {
+//       const [data2] = JSON.parse(request2.responseText);
+//       console.log(data2);
 
-      // Render neighbour country
-      renderCountry(data2, 'neighbour');
-    });
-  });
+//       // Render neighbour country
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// // getCountryAndNeighbour('portugal');
+// getCountryAndNeighbour('canada');
+
+////////////////////////////////////////////////////////////////
+
+// PROMISES AND FETCH //////////////////////////////////////////
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
 };
 
-// getCountryAndNeighbour('portugal');
-getCountryAndNeighbour('canada');
+getCountryData('portugal');
